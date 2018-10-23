4027e2cefd2961e268aca499c2ecc4a3d9179c52
Saeid Rezaei Baghbidi <saeid@Saeids-MacBook-Pro-2347.local>
2018-10-23 13:55:03 -0700

    Demo Microservices

---
+++ json-rest-api-cors-enabled/index.js
@@ -0,0 +1,69 @@
+/*
+ * *******************************************************************************
+ *   Copyright (c) 2018 Edgeworx, Inc.
+ *
+ *   This program and the accompanying materials are made available under the
+ *   terms of the Eclipse Public License v. 2.0 which is available at
+ *   http://www.eclipse.org/legal/epl-2.0
+ *
+ *   SPDX-License-Identifier: EPL-2.0
+ * *******************************************************************************
+ */
+
+const http = require('http')
+const ioFogClient = require('@iofog/nodejs-sdk')
+
+const PORT = 80
+
+let lastMessage = {}
+
+ioFogClient.init('iofog', 54321, null, () => {
+  ioFogClient.wsControlConnection(
+    {
+      'onNewConfigSignal': () => { },
+      'onError': (error) => {
+        console.error('There was an error with Control WebSocket connection to ioFog: ', error)
+      },
+    }
+  )
+
+  ioFogClient.wsMessageConnection(() => { }, {
+    'onMessages': (messages) => {
+      if (messages && messages.length) {
+        lastMessage = messages[0]
+      }
+    },
+    'onMessageReceipt': (messageId, timestamp) => { },
+    'onError': (error) => {
+      console.error('There was an error with Message WebSocket connection to ioFog: ', error)
+    }
+  })
+})
+
+
+const server = http.createServer((request, response) => {
+  switch (request.method) {
+    case 'OPTIONS': {
+      response.writeHead(200, {
+        'Access-Control-Allow-Credentials' : true,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Access-Control-Allow-Headers':'Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With',
+      })
+      response.end()
+      break
+    }
+    case 'GET': {
+      const base64 = lastMessage.contentdata
+      const data = (new Buffer(base64, 'base64')).toString('utf8')
+      response.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      })
+      response.end(data)
+      break
+    }
+  }
+})
+
+server.listen(PORT, () => { })
---
+++ sensors-data/src/index.ts
@@ -0,0 +1,61 @@
+import * as fs from 'fs'
+import * as parse from 'csv-parse'
+import * as path from 'path'
+
+import * as ioFogClient from '@iofog/nodejs-sdk'
+
+let time = 69000
+debugger
+
+function read(client): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const parser = parse({
+      delimiter: ','
+    })
+
+    parser.on('readable', async () => {
+      let data = parser.read()
+      while (data) {
+        const ms = (data[0] * 1000) - time
+        await sleep(ms)
+
+        time = (data[0] * 1000)
+        const json = {
+          time: Date.now(),
+          speed: data[1] * 2.23694,
+          acceleration: data[4],
+          rpm: data[5],
+        }
+
+        const ioMessage = client.ioMessage()
+        ioMessage.contentdata = Buffer.from(JSON.stringify(json)).toString('base64')
+        ioMessage.infotype = 'application/json'
+        ioMessage.infoformat = 'text/utf-8'
+        client.sendNewMessage(ioMessage, {
+          onMessageReceipt: () => { },
+          onBadRequest: (error) => { console.log('Error sending message', error) },
+          onError: (error) => { console.log('Error sending message', error) },
+        })
+
+        data = parser.read()
+      }
+    }).on('end', () => resolve())
+
+    fs.createReadStream(path.resolve(__dirname, '../data/trip-data.csv'))
+      .pipe(parser)
+  })
+}
+
+function sleep(ms: number): Promise<void> {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  })
+}
+
+ioFogClient.init('iofog', 54321, null, async () => {
+  while (true) {
+    await read(ioFogClient)
+  }
+})
